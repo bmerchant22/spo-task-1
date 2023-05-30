7123d0aa860e113e2fb3ad4796f6b624f3436df1
bmerchant <bmerchant22@iitk.ac.in>
2023-05-30 19:11:12 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div class="md:h-screen grid grid-cols-3 divide-x overflow-hidden">
+      <div className='col-span-3 outline-double sm:col-span-2'>
+        Quis amet dolor nostrud labore dolor nulla cupidatat. Est occaecat sunt anim Lorem aliquip aute fugiat culpa ullamco dolore proident cupidatat. Commodo voluptate ut dolor consectetur velit laboris est. Ad nostrud consequat aute sit aute veniam.Id anim et minim ad in laborum ea. Reprehenderit proident nostrud aliquip eu. Officia cupidatat reprehenderit est mollit dolor reprehenderit adipisicing excepteur mollit aliqua occaecat non adipisicing.
+
+        Et incididunt veniam voluptate elit occaecat. Adipisicing consectetur et fugiat ipsum minim exercitation dolor veniam occaecat. Aliquip reprehenderit veniam et non ipsum. Nostrud proident do officia incididunt duis aliquip ullamco aute Lorem minim.
+
+        Qui duis laboris magna consequat. Nostrud aliqua anim proident ea officia culpa. Sunt voluptate mollit velit exercitation reprehenderit fugiat. Est elit est ad deserunt velit et cillum labore minim. Aliquip proident aute adipisicing dolor fugiat ipsum eiusmod do officia. Ea esse id nulla mollit esse fugiat duis eiusmod velit laborum commodo proident sint.
+
+        Pariatur aliquip labore duis laborum in dolor quis aliqua sit deserunt. Elit voluptate commodo aliquip aliquip ad. Laboris deserunt veniam consequat duis adipisicing consectetur minim laboris est adipisicing culpa eu. Tempor magna ut eiusmod in.
+      </div>
+      <div className='col-span-3 outline-double sm:col-span-1'>
+        Reprehenderit do nisi eu duis voluptate irure id quis ut exercitation. Elit consectetur enim deserunt laborum deserunt id officia aliqua exercitation. Aute consequat laborum qui commodo commodo do id est in Lorem. Ipsum culpa anim nulla culpa qui qui. Nulla tempor Lorem laborum sint veniam quis enim. Consectetur voluptate eiusmod aute esse anim minim ea reprehenderit mollit incididunt duis.
+
+        Reprehenderit officia nulla ex amet quis non pariatur eu pariatur quis dolore. Elit fugiat proident dolore officia veniam et. Et pariatur ut irure est adipisicing voluptate commodo. Adipisicing Lorem sit excepteur dolore sunt aliquip cupidatat.
+      </div>
+      <div className='col-span-3 outline-double sm:col-span-1'>
+        Laborum ut nisi eu ipsum pariatur eiusmod. Nulla incididunt pariatur proident sunt nostrud laborum est ea aliqua ad sunt proident officia. Velit in irure est laboris laborum excepteur officia culpa aute aute. Eiusmod minim officia ut amet Lorem irure Lorem ipsum et pariatur id deserunt eu. Aute et anim Lorem laboris irure anim ut ut labore et exercitation. Dolore ad ipsum pariatur excepteur in labore.
+
+        Adipisicing dolor nostrud exercitation non laboris. Amet est enim elit in nulla nisi mollit quis sit. 
+      </div>
+      <div className='col-span-3 outline-double sm:col-span-1'>
+        Non culpa commodo irure minim nulla amet irure quis quis et enim occaecat dolor. Anim aliqua excepteur et tempor culpa cupidatat ipsum in nostrud quis minim. Cupidatat aliqua quis est eu aute incididunt sint id elit elit cupidatat pariatur voluptate. Irure do pariatur consectetur commodo cillum do occaecat nisi aute nulla do tempor. Dolore anim officia nisi Lorem sint adipisicing nulla irure veniam. Officia labore irure ea veniam cillum sunt adipisicing ullamco proident nulla adipisicing cillum. Aliquip ullamco enim amet adipisicing.
+
+        Anim ea reprehenderit deserunt elit ea et dolore in duis. Aliquip aute aliqua dolor cupidatat. Mollit proident
+      </div>
+      <div className='col-span-3 outline-double sm:col-span-1'>
+        Est id fugiat veniam dolor Lorem sit eiusmod Lorem cillum sit duis nostrud. Reprehenderit non aute duis ad quis amet. Ut commodo officia ad minim aute quis veniam sint amet fugiat nostrud excepteur. Ipsum ipsum quis dolore Lorem magna consectetur veniam tempor eu elit ea reprehenderit tempor. Irure occaecat ipsum nisi deserunt. Magna esse adipisicing cupidatat elit. Commodo Lorem culpa id commodo.
+
+        Enim nulla laboris laborum mollit incididunt anim mollit dolore aliquip sit nisi. Do voluptate proident culpa elit laborum elit voluptate sit. Nostrud laboris duis elit magna. Tempor velit ut commodo non nostrud. Proident esse fugiat nostrud duis. Est nulla magna sit est ullamco dolor ipsum. Excepteur 
+      </div>
+      <div className='col-span-3 outline-double sm:col-span-3'>
+        Proident veniam pariatur dolore commodo magna proident do. Ex incididunt esse esse aute laborum. Adipisicing ipsum quis ad exercitation dolore consectetur et proident ut labore. Officia nisi ut reprehenderit enim aliquip incididunt. Occaecat consectetur excepteur velit consectetur duis irure mollit magna.
+
+        Incididunt ad non nulla cupidatat occaecat magna ex. Adipisicing esse sit id eiusmod. Cillum laborum commodo ea pariatur nisi proident. Ullamco eiusmod laboris anim deserunt minim proident ullamco ea veniam Lorem minim ut tempor consequat. Irure fugiat quis est non commodo laborum aliqua consectetur magna. Laboris enim exercitation ad magna et nostrud velit nostrud aliqua magna ut irure.
+
+        Non esse ea nostrud ex commodo mollit reprehenderit. Culpa laboris eiusmod mollit consequat pariatur officia dolore occaecat dolor nulla. Irure officia eiusmod laborum labore consectetur nulla ad aliqua consequat laboris magna velit. Excepteur aliquip duis ut incididunt nisi ad velit dolore et culpa dolore qui deserunt excepteur. Labore dolore elit officia irure ut ea aliqua laborum amet officia. Ut nulla enim labore ipsum enim do irure commodo voluptate irure. Sunt exercitation incididunt amet fugiat.
+
+        Culpa aliquip aliqua laborum dolor commodo velit magna ullamco dolor incididunt quis eiusmod. Ea nisi magna tempor est pariatur ipsum incididunt velit. Aute amet Lorem do sit sunt voluptate elit nostrud enim. Cupidatat nostrud deserunt nulla ex aliquip.
+
+      </div>
     </div>
+    </>
   );
 }
 
